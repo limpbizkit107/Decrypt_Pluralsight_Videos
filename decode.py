ac6cb256a06d7a6464b02d6be03d8cbcb99c54e7
Stefan Minchev <stefan.minchev@sap.com>
2019-01-04 18:51:10 +0200

    Decryping is now working

--- decode.py
+++ decode.py
@@ -1,25 +1,14 @@
 import os
 import sqlite3
 from os.path import join, basename, normpath
-from shutil import copyfile
 
-def deobs_pluralsight(fpath, target_dir):
-    fname = os.path.basename(fpath)
-    target_fname = fname.replace('psv', 'mp4')
+def deobs_pluralsight(fpath, target_dir, target_fname):
     target_file_path = os.path.join(target_dir, target_fname)
 
     with open(target_file_path, "wb") as ofh:
         for byte in bytearray(open(fpath, "rb").read()):
             ofh.write(chr(byte ^ 101))
 
-
-def deobfuscate(arg,dirname,fnames): 
-    if fnames:
-        for fname in fnames:
-            fpath = os.path.join(dirname, fname)
-            print fpath
-            #deobs_pluralsight(fpath, target_dir)
-            
 def connect_to_databese(path_to_database):
     conn = sqlite3.connect(path_to_database)
     c = conn.cursor()
@@ -32,7 +21,9 @@ def connect_to_databese(path_to_database):
         for module in course_modules:
             clips = list(c.execute('SELECT ZID, ZTITLE FROM ZCLIPCD WHERE Z_FOK_MODULE = '+str(module[1])))
             for clip in clips:
-                copyfile(source_dir+clip[0]+".psv",course_name+"/"+module[0]+"/" )
+                print "Start decryping: " +clip[0].replace("-","")+".psv" +" --> "+clip[1]+".mp4"
+                #copy(source_dir+clip[0].replace("-","")+".psv","./"+course_name+"/"+module[0]+"/"+clip[1]+".psv")
+                deobs_pluralsight(source_dir+clip[0].replace("-","")+".psv","./"+course_name+"/"+module[0]+"/",clip[1]+".mp4")
     conn.close()
 
 def create_course_structute(course, modules):
@@ -43,9 +34,9 @@ def create_course_structute(course, modules):
         if not os.path.isdir(course+"/"+ module[0]):
             os.makedirs(course+"/"+ module[0])
 
-source_dir = "/Users/macbookpro/Library/Application Support/com.pluralsight.pluralsight-mac/ClipDownloads/"
-target_dir = "/Users/macbookpro/Desktop/Videos/"
-path_to_database = '/Users/macbookpro/Library/Application Support/com.pluralsight.pluralsight-mac/Model'
+source_dir = "/Users/i337566/Library/Application Support/com.pluralsight.pluralsight-mac/ClipDownloads/"
+target_dir = "/Users/i337566/Desktop/Videos/"
+path_to_database = '/Users/i337566/Library/Application Support/com.pluralsight.pluralsight-mac/Model'
 
 # if not os.path.isdir(target_dir):
 #     os.makedirs(target_dir)
