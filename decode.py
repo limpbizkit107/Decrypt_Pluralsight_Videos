e64a38abca583470c398d190da78f8745f722263
stefan michev <>
2019-01-07 00:33:15 +0200

    The video files are now correctly placed and the course structure is ordered

--- decode.py
+++ decode.py
@@ -9,39 +9,43 @@ def deobs_pluralsight(fpath, target_dir, target_fname):
         for byte in bytearray(open(fpath, "rb").read()):
             ofh.write(chr(byte ^ 101))
 
-def connect_to_databese(path_to_database):
+def extract_videos(path_to_database):
     conn = sqlite3.connect(path_to_database)
     c = conn.cursor()
-    print 'conection to the database was sucssefull'
-    courses = list(c.execute('SELECT ZTITLE, Z_PK FROM ZCOURSEHEADERCD'))
-    for course in courses:
-        course_name = course[0]
-        course_modules = list(c.execute('SELECT ZTITLE, Z_FOK_COURSE FROM ZMODULECD WHERE ZCOURSE = '+str(course[1])))
-        create_course_structute(course_name, course_modules)
-        for module in course_modules:
-            clips = list(c.execute('SELECT ZID, ZTITLE FROM ZCLIPCD WHERE Z_FOK_MODULE = '+str(module[1])))
-            for clip in clips:
-                print "Start decryping: " +clip[0].replace("-","")+".psv" +" --> "+clip[1]+".mp4"
-                #copy(source_dir+clip[0].replace("-","")+".psv","./"+course_name+"/"+module[0]+"/"+clip[1]+".psv")
-                deobs_pluralsight(source_dir+clip[0].replace("-","")+".psv","./"+course_name+"/"+module[0]+"/",clip[1]+".mp4")
+    print ('Connection to the database was successful')
+    dowloaded_courses = list(c.execute('SELECT ZHEADER FROM ZCOURSEDETAILSCD WHERE ZDOWNLOADED = 1'))
+    for downloaded_course in dowloaded_courses:
+        courses = list(c.execute('SELECT ZTITLE, Z_PK FROM ZCOURSEHEADERCD WHERE Z_PK = '+str(downloaded_course[0])))
+        for course in courses:
+            course_name = course[0]
+            course_modules = list(c.execute('SELECT ZTITLE, Z_PK FROM ZMODULECD WHERE ZCOURSE = '+str(course[1]) +' ORDER BY Z_FOK_COURSE ASC' ))
+            create_course_dir_structute(course_name, course_modules)
+            for module in course_modules:
+                clips = list(c.execute('SELECT ZID, ZTITLE FROM ZCLIPCD WHERE ZMODULE = '+str(module[1])+' ORDER BY Z_FOK_MODULE ASC'))
+                index = 1;
+                for clip in clips:
+                    print ("Start decrypting: " +clip[0].replace("-","")+".psv" +" --> "+target_dir+"/"+course_name+"/"+module[0]+"/"+" --> "+str(index)+"."+clip[1]+".mp4")
+                    deobs_pluralsight(source_dir+clip[0].replace("-","")+".psv",target_dir+"/"+course_name+"/"+module[0]+"/", str(index)+"."+clip[1]+".mp4")
+                    index+=1
     conn.close()
 
-def create_course_structute(course, modules):
-    print "creating folder structure for course: "+course
-    if not os.path.isdir(course):
-        os.makedirs(course)
+def create_course_dir_structute(course, modules):
+    print ("creating folder structure for course: "+course)
+    if not os.path.isdir(target_dir+course):
+        os.makedirs(target_dir+course)
+    index = 1;
     for module in modules:
-        if not os.path.isdir(course+"/"+ module[0]):
-            os.makedirs(course+"/"+ module[0])
+        if not os.path.isdir(target_dir+course+"/"+str(index)+"."+module[0]):
+            os.makedirs(target_dir+course+"/"+str(index)+"."+ module[0])
+            lst = list(module)
+            lst[0] = str(index)+"."+module[0]
+            modules[index-1] = tuple(lst)
+            index+=1
 
-source_dir = "/Users/i337566/Library/Application Support/com.pluralsight.pluralsight-mac/ClipDownloads/"
-target_dir = "/Users/i337566/Desktop/Videos/"
-path_to_database = '/Users/i337566/Library/Application Support/com.pluralsight.pluralsight-mac/Model'
+source_dir = "/Users/macbookpro/Library/Application Support/com.pluralsight.pluralsight-mac/ClipDownloads/"
+target_dir = "/Volumes/Stefan/Pluralsight/"
+path_to_database = '/Users/macbookpro/Library/Application Support/com.pluralsight.pluralsight-mac/Model'
 
-# if not os.path.isdir(target_dir):
-#     os.makedirs(target_dir)
-    
-# os.path.walk(source_dir, deobfuscate, None)
-connect_to_databese(path_to_database)
+extract_videos(path_to_database)
 
 
